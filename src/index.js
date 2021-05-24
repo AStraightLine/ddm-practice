8c7e53e5eede65e58445abe00c062d0190229b14
Lee Rogers <lrogersgm@gmail.com>
2021-05-24 08:40:18 +0100

    Prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const exploreNav = document.getElementById('navItemExplore');
+const exploreNav = document.getElementById('navItemExplore')
 
 const exploreNavHandler = () => {
-    const exploreDD = document.getElementById('exploreDD');
+    const exploreDD = document.getElementById('exploreDD')
 
     if (exploreDD.classList.contains('navItemOpen')) {
-        exploreNav.classList.remove('navItemClicked');
-        exploreDD.classList.remove('navItemOpen');
-    } else if (!(exploreNav.classList.contains('navItemOpen'))) {
-        exploreNav.classList.add('navItemClicked');
-        exploreDD.classList.add('navItemOpen');
+        exploreNav.classList.remove('navItemClicked')
+        exploreDD.classList.remove('navItemOpen')
+    } else if (!exploreNav.classList.contains('navItemOpen')) {
+        exploreNav.classList.add('navItemClicked')
+        exploreDD.classList.add('navItemOpen')
     }
-};
+}
 
-exploreNav.addEventListener('click', exploreNavHandler);
+exploreNav.addEventListener('click', exploreNavHandler)
